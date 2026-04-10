f6d0c3ef57899a744d04599104ace1bfff674bff
Jason Shan <jjasonsshan@gmail.com>
2026-04-10 15:59:34 +0200

    Add simple strikethrough animation for when task is completed

--- Assets/Scripts/Tasks/CurrentTaskUI.cs
+++ Assets/Scripts/Tasks/CurrentTaskUI.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class CurrentTaskUI : MonoBehaviour
 {
+    [SerializeField] private float strikethroughTime;
     private TextMeshProUGUI currentTaskText;
 
     void Awake()
@@ -26,11 +28,46 @@ public class CurrentTaskUI : MonoBehaviour
 
     void UpdateTaskText(TaskData task)
     {
-        currentTaskText.text = task.Description + " <b>" + task.Progress + "</b>";
+        SetTaskText(task.Description + " ", task.Progress);
     }
 
-    void CompleteTask(TaskData _)
+    void CompleteTask(TaskData task)
     {
-        currentTaskText.text = "<s>" + currentTaskText.text + "</s>";
+        StartCoroutine(AnimateStrikethrough(task.Description + " ", task.Progress));
+    }
+
+    void SetTaskText(string beginning, string bolded)
+    {
+        currentTaskText.text = beginning + "<b>" + bolded + "</b>";
+    }
+
+    IEnumerator AnimateStrikethrough(string description, string progress)
+    {
+        int endTagIdx = 1;
+        const string beginningTag = "<s>";
+        const string endTag = "</s>";
+        string newDescription = description;
+
+        float delay = strikethroughTime / (description.Length + progress.Length);
+
+        while (endTagIdx <= description.Length)
+        {
+            newDescription = beginningTag + description.Insert(endTagIdx, endTag);
+            SetTaskText(newDescription, progress);
+            yield return new WaitForSeconds(delay);
+
+            endTagIdx++;
+        }
+
+        endTagIdx = 1;
+
+        while (endTagIdx <= progress.Length)
+        {
+            string newProgress = beginningTag + progress.Insert(endTagIdx, endTag);
+            SetTaskText(newDescription, newProgress);
+            yield return new WaitForSeconds(delay);
+
+            endTagIdx++;
+        }
     }
 }
