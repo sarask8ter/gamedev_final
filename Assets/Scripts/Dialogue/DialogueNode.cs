b3857c703ff1de64d63c47f7b1aa5808717cb59b
Sara Ota <sota2491@gmail.com>
2026-04-15 20:45:32 +0200

    Dialogye system working

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueNode", menuName = "Scriptable Objects/DialogueNode")]
+public class DialogueNode : ScriptableObject
+{
+    [TextArea]
+    public string text;
+
+    public DialogueNode next;
+
+    public DialogueChoice[] choices;
+
+    public bool autoProgress;
+    public float autoDelay = 1.5f;
+}
