07a5df4bfa3d36a188e9d4d6f2bdc7066b5506d9
Jason Shan <jjasonsshan@gmail.com>
2026-04-30 16:51:12 +0200

    Finalize intro screen

--- Assets/Scripts/Miscellaneous/IntroScreen.cs
+++ Assets/Scripts/Miscellaneous/IntroScreen.cs
@@ -1,17 +1,18 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class IntroScreen : MonoBehaviour
 {
-    [SerializeField] GameObject introPanel;
-    [SerializeField] private float startDelay;
+    [SerializeField] CanvasGroup introPanel;
     [SerializeField] private float fadeDuration;
     private bool started;
 
     void Start()
     {
-        introPanel.SetActive(true);
+        introPanel.gameObject.SetActive(true);
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
@@ -27,20 +28,33 @@ public class IntroScreen : MonoBehaviour
         {
             // Start Game.
             started = true;
-    
-            introPanel.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            CoroutineHelper.Delay(startDelay, () => StartCoroutine(FadeInGameStart()));
+            StartCoroutine(FadeInGameStart());
         }
     }
 
     IEnumerator FadeInGameStart()
     {
         PlayerStateManager.State = PlayerState.NoInput;
+        yield return FadeCanvas();
+        introPanel.gameObject.SetActive(false);
         yield return ScreenFader.FadeIn(fadeDuration);
         PlayerStateManager.State = PlayerState.Normal;
         ProgressManager.CompleteEvent(ProgressEvent.GameStart);
         gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeCanvas()
+    {
+        float startAlpha = introPanel.alpha;
+
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            introPanel.alpha = Mathf.Lerp(startAlpha, 0, t / fadeDuration);
+            yield return null;
+        }
+
+        introPanel.alpha = 0;
     }
 }
