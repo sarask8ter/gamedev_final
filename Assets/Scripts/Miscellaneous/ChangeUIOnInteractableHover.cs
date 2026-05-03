684bbdd7ea01c9d407b7e2482fba4da0744d1edd
Jason Shan <jjasonsshan@gmail.com>
2026-05-03 20:14:23 +0200

    Add more keybindings

--- Assets/Scripts/Miscellaneous/ChangeUIOnInteractableHover.cs
+++ Assets/Scripts/Miscellaneous/ChangeUIOnInteractableHover.cs
@@ -5,16 +5,19 @@ public class ChangeUIOnInteractableHover : MonoBehaviour
 {
     [SerializeField] private GameObject hoverElement;
     [SerializeField] private PlayerState[] showHoverStates;
+    [SerializeField] private GameObject interactInstructionsUI;
 
     void Update()
     {
         if (!showHoverStates.Contains(PlayerStateManager.State)) {
             hoverElement.SetActive(false);
+            interactInstructionsUI.SetActive(false);
             return;
         };
 
         var interactable = PlayerInteractor.RaycastInteractable();
         var isHoveringInteractable = interactable != null && interactable.IsInteractable;
         hoverElement.SetActive(isHoveringInteractable);
+        interactInstructionsUI.SetActive(isHoveringInteractable);
     }
 }
